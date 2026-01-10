1f9f4fc8aad6b8b6b508d48a627547f05f63e3a5
Andrea C.E. <andreacolest@gmail.com>
2026-01-10 19:20:10 +0100

    Revert "QandA"

--- codigo/assets/AppAsset.php
+++ codigo/assets/AppAsset.php
@@ -21,7 +21,7 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     
     public $css = [
-        'css/site.css',    // Estilos base
+        '//css/site.css',    // Estilos base
         'css/styles.css',  // Estilos pagina
     ];
     
